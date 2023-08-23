e598dd4e25f839bdba9cada97648eb2fe13533f2
suzandev <dev.suzanchandra@gmail.com>
2023-08-24 00:27:39 +0600

    add some practice task

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ console.log(user.address.street.second);
 // const result = products.map((product) => product.name);
 // console.log(result);
 
+/*
 // =-=-=-=-=-=-= class methods start =-=-=-=-=-=-=
 
 class Teacher {
@@ -87,3 +88,100 @@ console.log(tapan);
 
 const rashad = new Teacher("Rashad", "Javascript");
 console.log(rashad);
+*/
+
+/*
+// example 2
+class Vehicle {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  move() {
+    console.log("gari chole na chole na chole na re");
+  }
+}
+
+class Bus extends Vehicle {
+  constructor(name, price, seat, ticketPrice) {
+    super(name, price);
+    this.seat = seat;
+    this.ticketPrice = ticketPrice;
+  }
+}
+
+class Truck extends Vehicle {
+  constructor(name, price, load) {
+    super(name, price);
+    this.load = load;
+  }
+}
+
+*/
+
+/*
+// practice task 1
+let data = {
+  sophia: {
+    id: 33,
+    study: [
+      {
+        primary: [
+          { school_name: "ABC primary school" },
+          { location: "peters burg" },
+        ],
+      },
+      {
+        secondary: [
+          { school_name: "ABC secondary school" },
+          { location: "Secondary school location" },
+        ],
+      },
+    ],
+  },
+};
+
+const result = data.sophia.study[1].secondary[1];
+
+console.log(result);
+
+*/
+
+/*
+
+// practice task 2
+let students = {
+  2222: {
+    name: "Jack",
+    section: "C",
+    class: "IX",
+    address: {
+      "building no": 12,
+      street: "St. Jonson",
+      city: "Petersburg",
+      country: "United States",
+    },
+  },
+};
+
+console.log(students[2222].address.city);
+
+*/
+
+/*
+
+// practice task 3
+const numbers = [1, 3, 5, 7, 9];
+
+const output = numbers.map((x) => x + 1);
+console.log(output);
+*/
+
+/*
+// practice task 4
+const deviated = [33, 50, 79, 78, 90, 101, 30];
+
+const outputResult = deviated.filter((x) => x % 10 === 0);
+console.log(outputResult);
+
+*/
